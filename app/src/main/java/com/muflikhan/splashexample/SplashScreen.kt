6239de8e5042101f40257a22bc97b31185b3e5cd
Muflikhan Dimas Dwiprayogi <muflikhan.dimas@gmail.com>
2022-03-15 13:47:52 +0700

    Initial commit

---
+++ app/src/main/java/com/muflikhan/splashexample/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.muflikhan.splashexample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val actionBar = supportActionBar
+        actionBar?.hide()
+
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 4000)
+    }
+}
